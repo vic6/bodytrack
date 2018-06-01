1746b637f96f1d207fca4cc185b09e446e309c64
vic6 <vicmatthews116@gmail.com>
2018-06-01 11:11:14 -0700

    Edit image upload form with more fields

--- client/src/components/AddPictureForm.js
+++ client/src/components/AddPictureForm.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-// import Dropzone from 'react-dropzone';
-import { Button } from 'react-bootstrap';
+import { Button, Col, ControlLabel, HelpBlock, FormGroup, FormControl } from 'react-bootstrap';
 
 export default class AddPictureForm extends Component {
-  state = { selectedFile: null, stats: { chest_size: '', waist_size: '' } };
-
-  setInputFormRef = element => {
-    this.inputForm = element;
+  state = {
+    selectedFile: null,
+    stats: { chest_size: '', waist_size: '', neck_size: '', hip_size: '', weight: '' }
   };
-
-  inputForm = null;
 
   handleFileChange = event => {
     this.setState({ selectedFile: event.target.files[0] });
@@ -18,23 +14,63 @@ export default class AddPictureForm extends Component {
   handleChange = event => {
     const { name, value } = event.target;
     this.setState({
-      stats: { [name]: value, waist_size: value }
+      stats: {...this.state.stats, [name]: value}
     });
-    console.log('Hi', this.state);
-    console.log(this.state.stats)
+    console.log(this.state.stats);
   };
 
+  FieldGroup = ({ id, label, help, ...props }) => (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+
   render() {
+    const { weight, neck_size, chest_size, hip_size, waist_size } = this.state;
     return (
       <div>
         <form
+          onChange={this.handleChange}
           onSubmit={event => this.props.readFile(event, this.state.selectedFile, this.state.stats)}>
-          <input
-            onChange={this.handleChange}
-            placeholder="Chest size"
+          <this.FieldGroup
+            id="formControlsText"
             type="number"
+            label="Weight"
+            placeholder="Weight"
+            name="weight"
+            value={weight}
+          />
+          <this.FieldGroup
+            id="formControlsText"
+            type="number"
+            label="Neck Size"
+            placeholder="Neck Size"
+            name="neck_size"
+            value={neck_size}
+          />
+          <this.FieldGroup
+            id="formControlsText"
+            type="number"
+            label="Chest Size"
+            placeholder="Chest Size"
             name="chest_size"
-            value={this.state.stats.chest_size}
+            value={chest_size}
+          />
+          <this.FieldGroup
+            label="Waist Size"
+            placeholder="Waist size"
+            type="number"
+            value={waist_size}
+            name="waist_size"
+          />
+          <this.FieldGroup
+            label="Hip Size"
+            placeholder="Hip size"
+            type="number"
+            value={hip_size}
+            name="hip_size"
           />
           <input
             ref={this.setInputFormRef}
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.css';
-import { Carousel } from 'react-responsive-carousel';
 import { PageHeader } from 'react-bootstrap';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+// import { Carousel } from 'react-responsive-carousel';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
 import AddPictureForm from './AddPictureForm';
 
 export default class Home extends Component {
-  state = { secondSnapStats: null };
-
-  getImage = event => {
-    console.log(event.target);
-    console.log('DATA', event.target.dataset);
-    // const el = document.getElementById('icecream')
-    // console.log(el.dataset.name);
-  };
 
   render() {
-    const { loaded } = this.props;
+    const { loaded, renderImages, readFile } = this.props;
 
     return (
       <div className="container">
         <PageHeader>Snapshots</PageHeader>
         {loaded ? (
           <div>
-            {this.props.renderImages()}
-            <p>{this.props.slide1}</p>
-            <p>{this.props.slide2}</p>
-            
-            <AddPictureForm readFile={this.props.readFile} />
+            {renderImages()}
+            <AddPictureForm readFile={readFile} />
           </div>
         ) : (
           <p>...Loading</p>
@@ -37,26 +26,3 @@ export default class Home extends Component {
     );
   }
 }
-
-/* <div>
-  <AddPictureForm readFile={this.readFile} />
-  {loaded ? this.renderImages() : <p>...Loading</p>}
-</div> */
-
-// <div style={{padding: '50px'}}>
-//   <AddPictureForm readFile={this.readFile} />
-//   <Grid container spacing={8}>
-//     <Grid item sm={6} xs={12}>
-//       <Paper sm={6} xs={12}>
-//       <Carousel dynamicHeight showThumbs={false}>
-//         {snapshots}
-//       </Carousel>
-//     </Paper>
-//     </Grid>
-//     <Grid item sm={6} xs={12}>
-//       <Carousel dynamicHeight showThumbs={false} selectedItem={1} >
-//         {snapshots}
-//       </Carousel>
-//     </Grid>
-//   </Grid>
-// </div>
