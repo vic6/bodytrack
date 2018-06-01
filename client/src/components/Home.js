afaaea02e431205ab0df9ef251594fc70ee7170f
vic6 <vicmatthews116@gmail.com>
2018-05-31 22:36:04 -0700

    Now able to access picture date from outside Carousel, add material table

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,57 +7,29 @@ import Paper from '@material-ui/core/Paper';
 import AddPictureForm from './AddPictureForm';
 
 export default class Home extends Component {
-  state ={ secondSnapStats: null }
+  state = { secondSnapStats: null };
 
-  getImage = (event) => {
-    console.log(event.target)
-    console.log('DATA',event.target.dataset)
+  getImage = event => {
+    console.log(event.target);
+    console.log('DATA', event.target.dataset);
     // const el = document.getElementById('icecream')
     // console.log(el.dataset.name);
-  }
+  };
 
   render() {
     const { loaded } = this.props;
-
 
     return (
       <div className="container">
         <PageHeader>Snapshots</PageHeader>
         {loaded ? (
-          <Grid container spacing={8}>
-            <Grid item sm={6} xs={12}>
-              <Paper sm={6} xs={12}>
-                {/* <Carousel onChange={this.props.logId} dynamicHeight showThumbs={false} infiniteLoop>
-                  <div onClick={(event)=>this.getImage(event)}>
-                    <img id='icecream'
-                      alt="snapshot"
-                      src="/uploads/snapshot/picture/1/nature.jpeg"
-                      max-width="20%"
-                      height="500px"
-                    />
-                  </div>
-                  <div onClick={(event)=>this.getImage(event)}>
-                    <img id='icecream'
-                      alt="snapshot"
-                      src="/uploads/snapshot/picture/1/nature.jpeg"
-                      max-width="20%"
-                      height="500px"
-                    />
-                  </div>
-                </Carousel> */}
-                {this.props.renderImages()}
-                {/* <p>{document.getElementById(1).getAttribute('key')}</p> */}
-                {/* <div>{this.renderImageData()}</div> */}
-              </Paper>
-            </Grid>
-            <Grid item sm={6} xs={12}>
-              {/* {this.props.renderImages()} */}
-              {/* <Carousel onChange={()=>{console.log('dude')}} dynamicHeight showThumbs={false} selectedItem={1} infiniteLoop>
-                {this.props.renderImages()}
-              </Carousel> */}
-            </Grid>
+          <div>
+            {this.props.renderImages()}
+            <p>{this.props.slide1}</p>
+            <p>{this.props.slide2}</p>
+            
             <AddPictureForm readFile={this.props.readFile} />
-          </Grid>
+          </div>
         ) : (
           <p>...Loading</p>
         )}
--- client/src/components/HomeContainer.js
+++ client/src/components/HomeContainer.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Home from './Home';
 import Auth from '../modules/Auth';
 
 export default class HomeContainer extends Component {
-  state = { snapshots: [], loaded: false, index: null };
+  state = { snapshots: [], loaded: false, index1: 0, index2: 0 };
 
   componentDidMount() {
     this.getUserSnapshots();
@@ -21,7 +28,7 @@ export default class HomeContainer extends Component {
       .then(res => res.json())
       .then(res => {
         console.log('RESPONSE', res);
-        this.setState({ snapshots: res.snapshots, index1: 0, index2: 0, loaded: true });
+        this.setState({ snapshots: res.snapshots, loaded: true });
       })
       .catch(errors => console.log(errors));
   };
@@ -64,40 +71,21 @@ export default class HomeContainer extends Component {
     // console.log(event.target.firstChild)
   };
 
-  myHandler1 = (index) => {
+  myHandler1 = index => {
     const currentIndex = index;
-    if (this.state.loaded){
-      this.setState({index1: currentIndex})
-      console.log(this.state)
+    if (this.state.loaded) {
+      this.setState({ index1: currentIndex });
+      console.log(this.state);
     }
-  }
+  };
 
-  myHandler2 = (index) => {
+  myHandler2 = index => {
     const currentIndex = index;
-    if (this.state.loaded){
-      this.setState({index2: currentIndex})
-      console.log(this.state)
+    if (this.state.loaded) {
+      this.setState({ index2: currentIndex });
+      console.log(this.state);
     }
-  }
-
-  logId = (e) => {
-    console.log(this.state.snapshots[e])
-    const details = this.state.snapshots[e];
-    if (details) {
-      return (
-        <div>
-          <p>{details.waist_size}</p>
-          <p>{details.chest_size}</p>
-        </div>
-      )
-    }
-    // return (
-    //   <div>
-    //     <p>{details.waist_size}</p>
-    //     <p>{details.chest_size}</p>
-    //   </div>
-    // )
-  }
+  };
 
   // renderImages = () =>
   //   this.state.snapshots.map(snap => (
@@ -107,110 +95,117 @@ export default class HomeContainer extends Component {
   //     </div>
   //   ));
 
-  renderImages = () =>
-     (
-      <div>
-        <Carousel id='carousel2' selectedItem={this.state.index1} showThumbs={false} onChange={this.myHandler1} infiniteLoop>
-          {this.state.snapshots.map(snap => (
-            <div
-              data-date={snap.created_at}
-              data-waist={snap.waist_size}
-              role="presentation"
-              onClick={event => this.logPicture(event)}
-              key={snap.id}
-              id={snap.id}>
-              <img alt="snapshot" src={snap.picture.url} max-width="20%" height="500px" />
-              <p className={this.state.active ? null : 'legend'}>
-                {snap.created_at.match('[^T]*')}
-              </p>
-            </div>
-          ))}
-        </Carousel>
-        <Carousel id='carousel2' selectedItem={this.state.index2} showThumbs={false} onChange={this.myHandler2} infiniteLoop>
-          {this.state.snapshots.map(snap => (
-            <div
-              data-date={snap.created_at}
-              data-waist={snap.waist_size}
-              role="presentation"
-              onClick={event => this.logPicture(event)}
-              key={snap.id}
-              id={snap.id}>
-              <img alt="snapshot" src={snap.picture.url} max-width="20%" height="500px" />
-              <p className={this.state.active ? null : 'legend'}>
-                {snap.created_at.match('[^T]*')}
-              </p>
-            </div>
-          ))}
-        </Carousel>
-      </div>
-    )
+  renderImages = () => {
+    const waist_size = this.state.sn
+    const {snapshots, index1, index2} = this.state
+    return (
+      <Grid container spacing={8}>
+        <Grid item sm={6} xs={12}>
+          <Paper sm={6} xs={12}>
+            <Carousel
+              id="carousel2"
+              selectedItem={index1}
+              showThumbs={false}
+              onChange={this.myHandler1}
+              infiniteLoop>
+              {this.state.snapshots.map(snap => (
+                <div key={snap.id} id={snap.id}>
+                  <img alt="snapshot" src={snap.picture.url} max-width="20%" height="500px" />
+                  <p className="legend">{snap.created_at.match('[^T]*')}</p>
+                </div>
+              ))}
+            </Carousel>
+          </Paper>
+        </Grid>
 
-  // renderImages = () => {
-  //   const snapShots = this.state.snapshots.map(snap => {
-  //     <div
-  //       data-date={snap.created_at}
-  //       data-waist={snap.waist_size}
-  //       role="presentation"
-  //       onClick={event => this.logPicture(event)}
-  //       key={snap.id}
-  //       id={snap.id}>
-  //       <img alt="snapshot" src={snap.picture.url} max-width="20%" height="500px" />
-  //       <p className={this.state.active ? null : 'legend'}>{snap.created_at.match('[^T]*')}</p>
-  //     </div>
-  //
-  //   }
-  //   );
-  //   return (
-  //     <div>
-  //     <Carousel
-  //       onChange={() => {
-  //         console.log('dude');
-  //       }}
-  //       dynamicHeight
-  //       showThumbs={false}
-  //       selectedItem={1}
-  //       infiniteLoop>
-  //       {snapShots}
-  //     </Carousel>
-  //     <p>I Rule</p>
-  //   </div>
-  //   );
-  // };
+        <Grid item sm={6} xs={12}>
+          <Paper sm={6} xs={12}>
+            <Carousel
+              id="carousel2"
+              selectedItem={index2}
+              showThumbs={false}
+              onChange={this.myHandler2}
+              infiniteLoop>
+              {this.state.snapshots.map(snap => (
+                <div
+                  data-date={snap.created_at}
+                  data-waist={snap.waist_size}
+                  role="presentation"
+                  onClick={event => this.logPicture(event)}
+                  key={snap.id}
+                  id={snap.id}>
+                  <img alt="snapshot" src={snap.picture.url} max-width="20%" height="500px" />
+                  <p className={this.state.active ? null : 'legend'}>
+                    {snap.created_at.match('[^T]*')}
+                  </p>
+                </div>
+              ))}
+            </Carousel>
+          </Paper>
+        </Grid>
 
-  renderImageData = () =>
-    this.state.snapshots.map(snap => (
-      <table key={snap.id}>
-        <tr>
-          <th>Neck Size</th>
-          <th>Chest Size</th>
-          <th>Waist Size</th>
-        </tr>
-        <tr>
-          <td>{snap.neck_size}</td>
-          <td>{snap.created_at}</td>
-          <td>{snap.waist_size}</td>
-        </tr>
-      </table>
-    ));
+        <Grid item sm={6} xs={12}>
+          <Paper sm={6} xs={12}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Measurement</TableCell>
+                  <TableCell>Result(inches)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell component="th" scope="row">Weight</TableCell>
+                  <TableCell>150</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Neck</TableCell>
+                  <TableCell>{snapshots[index1].neck_size || 0}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Waist</TableCell>
+                  <TableCell>{snapshots[index1].waist_size || 0}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Hip</TableCell>
+                  <TableCell>{snapshots[index1].hip_size || 0}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>URL</TableCell>
+                  <TableCell>{this.state.snapshots[this.state.index1].picture.url}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+
+        <Grid item sm={6} xs={12}>
+          <Paper sm={6} xs={12}>
+            {this.state.snapshots[this.state.index2].picture.url}
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  };
+
   render() {
-    const { loaded, snapshots, index1, index2 } = this.state
+    const { loaded, snapshots, index1, index2 } = this.state;
     const currentSlide1 = loaded ? snapshots[index1].picture.url : '';
     const currentSlide2 = loaded ? snapshots[index2].picture.url : '';
 
     return (
       <div>
         <Home
-          logId={this.logId}
           loaded={this.state.loaded}
           renderImages={this.renderImages}
-          renderImageData={this.renderImageData}
           readFile={this.readFile}
-          currentSlide={this.myhander}
+          slide1={currentSlide1}
+          slide2={currentSlide2}
         />
-        <div>
+        {/* <div>
           <h3>{currentSlide1}</h3>
           <h3>{currentSlide2}</h3>
-        </div>
+        </div> */}
       </div>
     );
   }
