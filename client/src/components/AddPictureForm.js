4d30ad90df3fb67bd954b82d87a2365414bd9008
vic6 <vicmatthews116@gmail.com>
2018-06-14 19:21:06 -0700

    Add button to toggle picture upload form

--- client/src/components/AddPictureForm.js
+++ client/src/components/AddPictureForm.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { Button, Col, ControlLabel, HelpBlock, FormGroup, FormControl, PageHeader } from 'react-bootstrap';
+import {
+  Button,
+  Col,
+  ControlLabel,
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  PageHeader
+} from 'react-bootstrap';
 import './AddPictureForm.css';
 
 export default class AddPictureForm extends Component {
   state = {
+    showForm: false,
     selectedFile: null,
     stats: { chest_size: '', waist_size: '', neck_size: '', hip_size: '', weight: '' }
   };
@@ -36,68 +45,94 @@ export default class AddPictureForm extends Component {
     </FormGroup>
   );
 
+  toggleForm = () => {
+    this.pageBottom.scrollIntoView();
+
+    this.setState({ showForm: !this.state.showForm });
+    this.scrollToBottom();
+  };
+
+  scrollToBottom = () => {
+    this.pageBottom.scrollIntoView();
+  };
+
   render() {
     const { weight, neck_size, chest_size, hip_size, waist_size, selectedFile, stats } = this.state;
     return (
-      <div className='picture-form'>
-        <PageHeader as='h2'>
-          Add Image
-        </PageHeader>
-        <Col xs={12} s={6}>
-          <form onSubmit={event => this.props.readFile(event, selectedFile, stats, this.resetFrom)}>
-            <this.FieldGroup
-              id="formControlsText"
-              type="number"
-              label="Weight"
-              placeholder="Weight"
-              name="weight"
-              value={weight}
-              onChange={this.handleChange}
-            />
-            <this.FieldGroup
-              id="formControlsText"
-              type="number"
-              label="Neck Size"
-              placeholder="Neck Size"
-              name="neck_size"
-              value={neck_size}
-              onChange={this.handleChange}
-            />
-            <this.FieldGroup
-              id="formControlsText"
-              type="number"
-              label="Chest Size"
-              placeholder="Chest Size"
-              name="chest_size"
-              value={chest_size}
-              onChange={this.handleChange}
-            />
-            <this.FieldGroup
-              label="Waist Size"
-              placeholder="Waist size"
-              type="number"
-              value={waist_size}
-              name="waist_size"
-              onChange={this.handleChange}
-            />
-            <this.FieldGroup
-              label="Hip Size"
-              placeholder="Hip size"
-              type="number"
-              value={hip_size}
-              name="hip_size"
-              onChange={this.handleChange}
-            />
-            <input
-              ref={this.setInputFormRef}
-              onChange={this.handleFileChange}
-              type="file"
-              accept="image/*"
-            />
-            {/* <input type="submit" name="Submit" /> */}
-            <Button bsStyle='primary' block type='submit'>Upload</Button>
-          </form>
-        </Col>
+      <div>
+        {!this.state.showForm ? (
+          <Button onClick={this.toggleForm}>Add Snapshot</Button>
+        ) : (
+          <div className="picture-form">
+            <PageHeader as="h2">Add Image</PageHeader>
+            <Col xs={10} s={6} m={8}>
+              <form
+                onSubmit={event => this.props.readFile(event, selectedFile, stats, this.resetFrom)}>
+                <this.FieldGroup
+                  id="formControlsText"
+                  type="number"
+                  label="Weight"
+                  placeholder="Weight"
+                  name="weight"
+                  value={weight}
+                  onChange={this.handleChange}
+                />
+                <this.FieldGroup
+                  id="formControlsText"
+                  type="number"
+                  label="Neck Size"
+                  placeholder="Neck Size"
+                  name="neck_size"
+                  value={neck_size}
+                  onChange={this.handleChange}
+                />
+                <this.FieldGroup
+                  id="formControlsText"
+                  type="number"
+                  label="Chest Size"
+                  placeholder="Chest Size"
+                  name="chest_size"
+                  value={chest_size}
+                  onChange={this.handleChange}
+                />
+                <this.FieldGroup
+                  label="Waist Size"
+                  placeholder="Waist size"
+                  type="number"
+                  value={waist_size}
+                  name="waist_size"
+                  onChange={this.handleChange}
+                />
+                <this.FieldGroup
+                  label="Hip Size"
+                  placeholder="Hip size"
+                  type="number"
+                  value={hip_size}
+                  name="hip_size"
+                  onChange={this.handleChange}
+                />
+                <input
+                  ref={this.setInputFormRef}
+                  onChange={this.handleFileChange}
+                  type="file"
+                  accept="image/*"
+                />
+                {/* <input type="submit" name="Submit" /> */}
+                <Button onClick={this.toggleForm} xs={6} type="submit">
+                  Cancel
+                </Button>
+                <Button xs={6} bsStyle="primary" type="submit">
+                  Upload
+                </Button>
+              </form>
+            </Col>
+          </div>
+        )}
+        <div style={{float:"left", clear:"both"}}
+          ref={el => {
+            this.pageBottom = el;
+          }}>hello
+        </div>
       </div>
     );
   }
