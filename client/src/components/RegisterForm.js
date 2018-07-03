96d00daabd2d0fc9e9359e52338615062f8500fb
vic6 <vicmatthews116@gmail.com>
2018-07-02 18:12:06 -0700

    Refactor RegisterFrom

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl } from 'react-bootstrap';
 
 export default class RegisterForm extends Component {
-  constructor() {
-    super();
-    this.state = { name: '', username: '', email: '', password: '' };
+  state = { name: '', username: '', email: '', password: '' };
 
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
+  handleChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
-  }
+  };
 
   render() {
     const { name, username, email, password } = this.state;
@@ -26,8 +21,8 @@ export default class RegisterForm extends Component {
             type="text"
             value={name}
             placeholder="Name"
-            onChange={this.handleChange}
             name="name"
+            onChange={this.handleChange}
           />
         </FormGroup>
         <FormGroup>
@@ -35,6 +30,7 @@ export default class RegisterForm extends Component {
             type="text"
             value={username}
             placeholder="Username"
+            name="username"
             onChange={this.handleChange}
           />
         </FormGroup>
@@ -42,6 +38,7 @@ export default class RegisterForm extends Component {
           <FormControl
             type="email"
             value={email}
+            name="email"
             placeholder="Email"
             onChange={this.handleChange}
           />
@@ -50,22 +47,12 @@ export default class RegisterForm extends Component {
           <FormControl
             type="password"
             value={password}
+            name="password"
             placeholder="Password"
             onChange={this.handleChange}
           />
         </FormGroup>
       </form>
-      // <div className="form">
-      //   <form
-      //     onChange={this.handleChange}
-      //     onSubmit={event => this.props.handleRegisterSubmit(event, this.state)}>
-      //     <input type="text" name="name" value={name} placeholder="Name" />
-      //     <input type="text" name="username" value={username} placeholder="Username" />
-      //     <input type="email" name="email" value={email} placeholder="email" />
-      //     <input type="password" name="password" value={password} placeholder="Password" />
-      //     <input type="submit" value="Register" />
-      //   </form>
-      // </div>
     );
   }
 }
