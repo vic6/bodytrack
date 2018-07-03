bd72a59b03742e2a1d7e386dd2ec7b2fef96f9c0
vic6 <vicmatthews116@gmail.com>
2018-07-02 18:08:05 -0700

    Add User validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   has_secure_password
   has_secure_token :auth_token
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
   has_many :characters
   has_many :snapshots
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'TestUser', username: 'Mr.Test',
+                     email: 'fake@email.com', password: 'password')
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
+  test "email should be present" do
+    @user.email = '    '
+    assert_not @user.valid?
+  end
+
+  test 'name does not exceed 50 charactors' do
+    @user.email = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test 'reject invalid email' do
+    invalid_addresses = %w[
+      user@example,com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com
+    ]
+
+    invalid_addresses.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect}, should be invalid"
+    end
+  end
+
+  test 'email is unique' do
+    user_duplicate = @user.dup
+    user_duplicate.email = @user.email.upcase
+    @user.save
+    assert_not user_duplicate.valid?
+  end
+
+  test 'email should be saved in lower-case' do
+    mixed_case_email = 'TESt@emaiL.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test 'password should not be blank' do
+    @user.password = @user.password_confirmation = '    '
+    assert_not @user.valid?
+  end
+
+  test 'password should contain at least 5 charactors' do
+    @user.password = @user.password_confirmation = 'abcd'
+    assert_not @user.valid?
+  end
+
+  test "authenticate? should return false for a user with nil digest" do
+    assert_not @user.authenticate('')
+  end
 end
