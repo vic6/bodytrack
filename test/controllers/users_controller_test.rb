80e26777a937ed7ebcd5bf87abbf38239c1641b8
vic6 <vicmatthews116@gmail.com>
2018-07-21 18:02:59 -0700

    Add rspec and User model tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(name: 'TestUser', username: 'Mr.Test',
+                        email: 'fake@email.com', password: 'password')
+  end
+
+  test "Unauthorized when not logged in" do
+    get home_path
+    assert_response 401
+  end
+
+  test '/home calls home action' do
+    assert_routing '/home', controller: 'users', action: 'home'
+  end
+
+  test "Logged in user directs to their homepage" do
+    require_login
+    p @user
+    get home_path(@user)
+    assert_response :success
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,55 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def require_login
+    authenticate_token || render_unauthorized('Access denied')
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  protected
+
+  def render_unauthorized(message)
+    errors = { errors: [detail: message] }
+    render json: errors, status: :unauthorized
+  end
+
+  def create
+    if (user = User.validate_login(params[:username], params[:password]))
+      allow_token_to_be_used_only_once_for(user)
+      send_token_for_valid_login_of(user)
+    else
+      render_unauthorized('Error with your username or password')
+    end
+  end
+
+  def destroy
+    logout
+    head :ok
+  end
+
+  private
+
+  def send_token_for_valid_login_of(user)
+    render json: { token: user.auth_token }
+  end
+
+  def allow_token_to_be_used_only_once_for(user)
+    user.regenerate_auth_token
+  end
+
+  def logout
+    current_user.invalidate_token
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      User.find_by(auth_token: token)
+    end
+  end
+  
 end
